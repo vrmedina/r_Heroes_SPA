f5f1f0f1de8ebba5afa319582f4ed38fb4bb1b49
Victor <vrmm1998@gmail.com>
2024-02-17 13:59:31 -0500

    finish search feature

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,5 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { useForm } from "../../hooks/useForm";
-import { HeroDetail } from "../components/HeroDetail";
 import queryString from "query-string";
 import { getHeroesByName } from "../helpers/getHeroesByName";
 import { HeroCard } from "../components/HeroCard";
@@ -11,13 +10,15 @@ export const SearchPage = () => {
   const { q = "" } = queryString.parse(location.search);
   const heroes = getHeroesByName(q);
 
+  const showSearch = q.length === 0;
+  const showError = q.length > 0 && heroes.length === 0;
+
   const { searchText, onInputChange } = useForm({
-    searchText: "",
+    searchText: q,
   });
 
   const onSearchSubmit = (evt) => {
     evt.preventDefault();
-    if (searchText.trim().length <= 1) return;
     navigate(`?q=${searchText}`);
   };
 
@@ -27,7 +28,7 @@ export const SearchPage = () => {
       <hr />
       <div className="card my-3" style={{ maxWidth: "1000px" }}>
         <div className="row g-0">
-          <div className="col-md-6">
+          <div className="col-xs-12 col-sm-5 col-md-5 col-lg-4">
             <div className="card-body">
               <h5 className="card-title">Search</h5>
               <hr />
@@ -45,18 +46,24 @@ export const SearchPage = () => {
               </form>
             </div>
           </div>
-          <div className="col-md-6">
+          <div className="col-xs-12 col-sm col-md-7 col-lg-8">
             <div className="card-body border-start border-secondary-subtle">
               <h5 className="card-title">Search results...</h5>
-              <div className="alert alert-secondary">
+              <div
+                className="alert alert-secondary animate__animated animate__headShake"
+                style={{ display: showSearch ? "" : "none" }}
+              >
                 <p className="p-0 m-0">Please search a Superhero first</p>
               </div>
-              <div className="alert alert-danger">
+              <div
+                className="alert alert-danger animate__animated animate__headShake"
+                style={{ display: showError ? "" : "none" }}
+              >
                 <p className="p-0 m-0">
                   We could not find a hero with name <b>{q}</b>
                 </p>
               </div>
-              <div className="row rows-cols-xs-1 row-cols-sm-1 row-cols-md-1 row-cols-lg-2 g-3">
+              <div className="row rows-cols-xs-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-3">
                 {heroes.map((hero) => (
                   <HeroCard key={hero.id} {...hero} />
                 ))}
