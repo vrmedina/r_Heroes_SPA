d60dbccf88486bbe2260db1c07e6a7b2592ab96d
Victor <vrmm1998@gmail.com>
2024-02-23 09:55:31 -0500

    add test to PrivateRoute

---
+++ tests/router/PublicRoute.test.jsx
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import { AuthContext } from "../../src/auth";
+import { PublicRoute } from "../../src/router/PublicRoute";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+
+describe("Pruebas en <PublicRoute />", () => {
+  test("debe mostrar children si no esta autenticado", () => {
+    const contextValue = { logged: false };
+
+    render(
+      <AuthContext.Provider value={contextValue}>
+        <PublicRoute>
+          <h1>Ruta publica</h1>
+        </PublicRoute>
+      </AuthContext.Provider>,
+    );
+
+    expect(screen.getByText("Ruta publica")).toBeTruthy();
+  });
+
+  test("debe llamar el Navigate si esta autenticado", () => {
+    const contextValue = {
+      logged: true,
+      user: {
+        id: "321",
+        name: "Testtest",
+      },
+    };
+  });
+});
